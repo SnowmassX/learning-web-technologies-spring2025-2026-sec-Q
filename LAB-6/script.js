151d5d928ce1b5e4ae03433cf171e43543b93dd2
SnowmassX <snowmass1056@gmail.com>
2026-03-13 22:19:38 +0600

    commit 3rd

--- LAB-6/script.js
+++ LAB-6/script.js
@@ -7,17 +7,17 @@ let cells = [];
 
 let display = document.getElementById("display-id");
 
-for(let i = 0; i < container.children.length; i++){
+for (let i = 0; i < container.children.length; i++) {
 
     let row = container.children[i];
 
-    for(let j = 0; j < row.children.length; j++){
+    for (let j = 0; j < row.children.length; j++) {
         cells.push(row.children[j]);
     }
 }
 
 
-for(let i = 0; i < cells.length; i++){
+for (let i = 0; i < cells.length; i++) {
 
     cells[i].addEventListener("click", function () {
         play(i);
@@ -26,7 +26,7 @@ for(let i = 0; i < cells.length; i++){
 
 function play(index) {
 
-    if(board[index] !== "" || gameOver){
+    if (board[index] !== "" || gameOver) {
         return;
     }
 
@@ -35,19 +35,19 @@ function play(index) {
 
     checkWinner();
 
-    if(!gameOver){
-        if(player === "X") {
+    if (!gameOver) {
+        if (player === "X") {
             player = "O";
-        } 
-        else{
+        }
+        else {
             player = "X";
         }
     }
 }
 
-function checkWinner(){
+function checkWinner() {
 
-    if(
+    if (
         (board[0] === board[1] && board[1] === board[2] && board[0] !== "") ||
         (board[3] === board[4] && board[4] === board[5] && board[3] !== "") ||
         (board[6] === board[7] && board[7] === board[8] && board[6] !== "") ||
@@ -58,24 +58,23 @@ function checkWinner(){
 
         (board[0] === board[4] && board[4] === board[8] && board[0] !== "") ||
         (board[2] === board[4] && board[4] === board[6] && board[2] !== "")
-    ) 
-    {
+    ) {
         display.innerText = player + " wins!";
         gameOver = true;
         return;
     }
 
-    if (!board.includes("")){
+    if (!board.includes("")) {
         display.innerText = "Draw!";
         gameOver = true;
     }
 }
 let resetBtn = document.getElementById("resetBtn");
 
-resetBtn.addEventListener("click", function (){
+resetBtn.addEventListener("click", function () {
     board = ["", "", "", "", "", "", "", "", ""];
 
-    for(let i = 0; i < cells.length; i++){
+    for (let i = 0; i < cells.length; i++) {
         cells[i].innerText = "";
     }
 
