7e830971cb1a7616126048274a838e55bfbead45
SnowmassX <snowmass1056@gmail.com>
2026-04-15 14:06:41 +0600

    lab-2 login.php

---
+++ FINAL/LAB TASK-2/forgot_password.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <a href="home.php">Home</a>
+        <a href="login.php">Login</a>
+        <a href="register.php">Registration</a>
+    </header>
+    <br><br><br>
+    <fieldset>
+        <legend>FORGOT PASSWORD</legend>
+        Enter Email : <input type="email" name="email"><br><hr>
+        <input type="submit" name="submit">
+    </fieldset><br><br><br>
+    <footer>
+        copyright 2017
+
+    </footer>
+</body>
+</html>
--- FINAL/LAB TASK-2/login.php
+++ FINAL/LAB TASK-2/login.php
@@ -14,11 +14,11 @@
     <fieldset>
         <legend>LOGIN</legend>
         User Name : <input type="text" name="login_name"><br>
-        Password : <input type="password" name="login_password"><br>
+        Password : <input type="password" name="login_password"><br><hr>
         <input type="checkbox" name="remember_me" id="" required> Remember Me <br>
         <input type="submit" name="login_submit">
         <a href="forgot_password.php">Forgot Password?</a>
-    </fieldset>
+    </fieldset><br><br><br>
     <footer>
         copyright 2017
 
